6dbc760b6163f00fa4943e9a146515337e2b988b
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2020-04-14 02:55:22 +0500

    printing basic user

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/chaudhryjunaid/go-webservice/models"
+)
 
 func main() {
-	fmt.Println("hello, world!")
+	u := models.User{
+		ID:        2,
+		FirstName: "Chaudhry",
+		LastName:  "Anwar",
+	}
+	fmt.Println(u)
 }
