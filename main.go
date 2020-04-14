4de8e4b05e6801589a0619d30c3b2557a019715f
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2020-04-14 12:35:33 +0500

    basic http server

--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
-	"github.com/chaudhryjunaid/go-webservice/models"
+	"github.com/chaudhryjunaid/go-webservice/controllers"
 )
 
 func main() {
-	u := models.User{
-		ID:        2,
-		FirstName: "Chaudhry",
-		LastName:  "Anwar",
-	}
-	fmt.Println(u)
+	controllers.RegisterControllers()
+	http.ListenAndServe(":3000", nil)
 }
